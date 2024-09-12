127c8f55c1bfd40a4931666419b5dd2cbe8d0c61
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-09-11 17:17:43 -0700

    updates

--- tictactoe/src/main/java/com/kiran/lld/tictactoe/models/Board.java
+++ tictactoe/src/main/java/com/kiran/lld/tictactoe/models/Board.java
@@ -2,23 +2,19 @@ package com.kiran.lld.tictactoe.models;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@Setter
 public class Board {
-    private int size;
-    private List<List<BoardCell>> cells = new ArrayList<>();
+    List<List<BoardCell>> cells = new ArrayList<>();
 
-    public Board(int size){
-        this.size = size;
-        this.cells = initializeCells(size);
-    }
-
-    private List<List<BoardCell>> initializeCells(int size) {
-        List<BoardCell> firstRow =  Collections.nCopies(size, new BoardCell());
-        return Collections.nCopies(size, firstRow);
+    public Board(int rows, int columns) {
+        List<BoardCell> columnCells = Collections.nCopies(columns, new BoardCell());
+        List<List<BoardCell>> rowCells = Collections.nCopies(rows, columnCells);
+        this.cells = rowCells;
     }
 }
--- tictactoe/src/test/java/com/kiran/lld/tictactoe/TictactoeApplicationTests.java
+++ tictactoe/src/test/java/com/kiran/lld/tictactoe/TictactoeApplicationTests.java
@@ -1,13 +1,29 @@
 package com.kiran.lld.tictactoe;
 
+import com.kiran.lld.tictactoe.models.Board;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TictactoeApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCreateGame() {
+
+	}
+
+	@Test
+	public void testCreateBoard() {
+		Board b = new Board(3, 3);
+		assertEquals(3, b.getCells().get(0).size(),
+				"If the ctor of board is called with n, it should create n rows"
+		);
 	}
 
 }
